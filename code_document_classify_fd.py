79a26411131ccd64c10c0ed2876c4c00f1f744f6
Ellen Fiscina <ellfiscina@gmail.com>
2018-10-15 18:03:19 -0300

    classification

---
+++ code_document_classify_fd.py
@@ -0,0 +1,17 @@
+# Natural Language Toolkit: code_document_classify_fd
+
+all_words = nltk.FreqDist(w.lower() for w in movie_reviews.words())
+word_features = list(all_words)[:2000] # [_document-classify-all-words]
+
+def document_features(document): # [_document-classify-extractor]
+    document_words = set(document) # [_document-classify-set]
+    features = {}
+    for word in word_features:
+        features['contains({})'.format(word)] = (word in document_words)
+    return features
+
+featuresets = [(document_features(d), c) for (d,c) in documents]
+train_set, test_set = featuresets[100:], featuresets[:100]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+
+# https://www.nltk.org/book/ch06.html
